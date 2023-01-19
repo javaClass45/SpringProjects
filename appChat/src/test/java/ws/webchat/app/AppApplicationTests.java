3b4315e54d8e137bb9e72e7046152c019f43cf14
Anton <javaClass45@gmail.com>
2023-01-19 11:51:57 +0200

    added WebSocket chat on Spring Boot

---
+++ appChat/src/test/java/ws/webchat/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.webchat.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
