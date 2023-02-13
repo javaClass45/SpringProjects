c19e1008a773aca63c5fd3565715c4a8cc322d5e
Anton <javaClass45@gmail.com>
2023-02-13 20:25:59 +0200

    new SprinBoot project for JUnit&Mock test good started

---
+++ junitmock/src/main/java/com/demo/junitmock/JunitmockApplication.java
@@ -0,0 +1,13 @@
+package com.demo.junitmock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitmockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunitmockApplication.class, args);
+    }
+
+}
