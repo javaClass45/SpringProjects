e6eee2bfdd350e9da0ed3b2e4ad02375994b9859
Anton <javaClass45@gmail.com>
2023-06-17 20:45:17 +0300

    downcast version of project to 11

--- mailsender/src/main/java/com/email/mailsender/MailsenderApplication.java
+++ mailsender/src/main/java/com/email/mailsender/MailsenderApplication.java
@@ -1,13 +1,13 @@
-package com.email.sender;
+package com.email.mailsender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MailsenderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MailsenderApplication.class, args);
 	}
 
 }
