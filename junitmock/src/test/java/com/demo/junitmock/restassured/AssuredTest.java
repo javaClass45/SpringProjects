e7e66f063e7f4bf0c02307a0a4cd8e5f611d78ca
Anton <javaClass45@gmail.com>
2023-12-11 17:09:05 +0200

    project APITest is closed, on erorr

--- junitmock/src/test/java/com/demo/junitmock/restassured/AssuredTest.java
+++ junitmock/src/test/java/com/demo/junitmock/restassured/AssuredTest.java
@@ -1,12 +1,43 @@
 package com.demo.junitmock.restassured;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+
+
 public class AssuredTest {
+
+
 
     @Test
     void restAssured() {
-        
+//        RestAssured
+//                .given()
+//                .log().all()
+//                .when()
+//                .contentType(ContentType.JSON)
+////                .body(orderReq)
+////                .post("http://localhost:8080/users/save")
+//                .then();
+
+
+    String orderReq = "{\"id\":4," +
+            "\"name\":\"ENKANTON NAZARO\"," +
+            "\"login\":\"ENKANTON\"," +
+            "\"email\":\"ENKANTON@mail.com\"}";
+
+
+
+        RestAssured
+                .given()
+                .log().all()
+                .when()
+                .contentType(ContentType.JSON)
+                .body(orderReq)
+                .post("http://localhost:8080/users/save")
+                .then()
+                .log().all()
+                    .statusCode(200);
     }
 
 }
